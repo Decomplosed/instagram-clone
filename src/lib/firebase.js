bb51a2cd74a5e30677a1ab089243efed2725f328
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-18 08:47:32 +0100

    added images

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,8 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+import { seedDatabase } from '../seed';
+
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,5 +15,7 @@ const config = {
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
+
+seedDatabase(firebase);
 
 export { firebase, FieldValue };
