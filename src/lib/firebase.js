cf5a2cca4a2214c93dd96cb1f276b912244d09a7
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-21 20:59:01 +0100

    commented out seeding

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,7 +2,7 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import { seedDatabase } from '../seed';
+// import { seedDatabase } from '../seed';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,6 +16,8 @@ const config = {
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
-seedDatabase(firebase);
+console.log(firebase);
+
+// seedDatabase(firebase);
 
 export { firebase, FieldValue };
