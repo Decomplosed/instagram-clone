9edb0c6481cd52355af8afb6690ea9e175fb6428
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-24 21:11:07 +0100

    imported FirebaseContext

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 
 const Login = () => {
   const history = useHistory();
