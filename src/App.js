9df1b1657920117392ca01528038f37a034c60bd
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-24 21:03:43 +0100

    imported Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const Login = lazy(() => import('./pages/Login'));
