ad4f89e7d04439eebaa24f930ca7e520a6e2e8ae
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-27 18:35:02 +0100

    show error dynamicall

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,6 +34,7 @@ const Login = () => {
             className='mt-2 w-6/12 mb-4'
           />
         </h1>
+        {error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
       </div>
     </div>
   );
