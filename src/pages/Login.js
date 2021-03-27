7295a8aba7b9a309653a6fe284e65516f3a42561
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-27 18:41:40 +0100

    render input

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -44,6 +44,7 @@ const Login = () => {
             className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
             onChange={({ target }) => setEmailAddress(target.value)}
           />
+          <input />
         </form>
       </div>
     </div>
