3e1d5e7e4a9e727d038471beb453e1139adf8843
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-27 18:43:02 +0100

    set password value on change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -49,6 +49,7 @@ const Login = () => {
             type='password'
             placeholder='Password'
             className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+            onChange={({ target }) => setPassword(target.value)}
           />
         </form>
       </div>
