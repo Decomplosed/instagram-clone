4e122f3f40fb31c435bfacac4980e159fd894aae
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-27 19:12:39 +0100

    imported Link

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 const Login = () => {
