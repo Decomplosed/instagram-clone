37976199ff5cc24984b4ff9568bdfd01bccc9769
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-27 19:09:21 +0100

    added basic theme temporarily

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,37 @@ module.exports = {
   purge: {
     content: ['./src/**/*.js', './src/**/**/*.js'],
   },
+  // theme: {
+  //   fill: (theme) => ({
+  //     red: theme('colors.red.primary'),
+  //   }),
+  //   colors: {
+  //     white: '#ffffff',
+  //   },
+  //   blue: {
+  //     medium: '#005c98',
+  //   },
+  //   black: {
+  //     light: '#262626',
+  //     faded: '#00000059',
+  //   },
+  //   gray: {
+  //     base: '#616161',
+  //     background: '#fafafa',
+  //     primary: '#dbdbdb',
+  //   },
+  //   red: {
+  //     primary: '#ed4956',
+  //   },
+  // },
   theme: {
-    fill: (theme) => ({
-      red: theme('colors.red.primary'),
-    }),
     colors: {
       white: '#ffffff',
-    },
-    blue: {
-      medium: '#005c98',
-    },
-    black: {
-      light: '#262626',
-      faded: '#00000059',
-    },
-    gray: {
-      base: '#616161',
-      background: '#fafafa',
-      primary: '#dbdbdb',
-    },
-    red: {
-      primary: '#ed4956',
-    },
-  },
+      blue: {
+            medium: '#005c98',
+          },
+    }
+  }
   variants: {
     extend: {
       display: ['group-hover'],
