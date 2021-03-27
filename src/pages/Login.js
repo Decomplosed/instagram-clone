297d29114e0f2ebe751a5101f743fde584cea55e
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-27 18:40:16 +0100

    added border to input and make it round

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
             aria-label='Enter your email address'
             type='text'
             placeholder='Email Address'
-            className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2'
+            className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
           />
         </form>
       </div>
