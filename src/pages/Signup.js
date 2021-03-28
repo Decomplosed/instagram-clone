9c876662edffec86f0179a31ed1ad3a5e64b9a3f
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-28 11:31:37 +0200

    declared Signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,3 +2,5 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
+
+const Signup = () => {};
