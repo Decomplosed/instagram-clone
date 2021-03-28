6bcdf93463aa4766b896cce31a84b230850c7dd6
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-28 11:35:09 +0200

    copied the body skeleton from Login page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -13,7 +13,32 @@ const Signup = () => {
     document.title = 'Sign Up - Instagram';
   }, []);
 
-  return <p>Signup</p>;
+  return (
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <div className='flex w-3/5'>
+        <img
+          src='/images/iphone-with-profile.jpg'
+          alt='iPhone with Instagram app'
+        />
+      </div>
+      <div className='flex flex-col w-2/5'>
+        <div className='flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded'>
+          <h1 className='flex justify-center w-full'>
+            <img
+              src='/images/logo.png'
+              alt='Instagram'
+              className='mt-2 w-6/12 mb-4'
+            />
+          </h1>
+
+          {error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
+
+          <form onSubmit={handleSignUp} method='POST'></form>
+        </div>
+        <div className='flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
