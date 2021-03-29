09d3f825bdb34f8a1a228e76e070be440f2ed0ce
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-29 13:37:20 +0200

    added where call

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,8 @@
 import { firebase, FieldValue } from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
-  const result = await firebase.firestore().collection('users');
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', username);
 }
