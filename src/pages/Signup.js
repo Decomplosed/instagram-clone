1e68e2b391640a83deb95587f95dcc34dedc1d8e
Bart Frackiewicz <decomplosed@gmail.com>
2021-03-29 13:40:37 +0200

    declared createdUserResult

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -22,6 +22,7 @@ const Signup = () => {
     const usernameExists = await doesUsernameExist(username);
     if (!usernameExists.length) {
       try {
+        const createdUserResult = await firebase;
       } catch (error) {}
     }
   };
