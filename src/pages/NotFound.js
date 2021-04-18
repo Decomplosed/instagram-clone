db8b082641e02b67569da91b2587d731fe62f823
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 09:39:08 +0200

    changed NotFOund to be array function

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function NotFound() {
-  return (
-    <div className='bg-gray-background'>
-      <div className='max-w-screen-lg mx-auto'>
-        <p className='text-2xl text-center'>404 | Not Found</p>
-      </div>
+const NotFound = () => (
+  <div className='bg-gray-background'>
+    <div className='max-w-screen-lg mx-auto'>
+      <p className='text-2xl text-center'>404 | Not Found</p>
     </div>
-  );
-}
+  </div>
+);
+
+export default NotFound;
