a0b62a521c190a0dacd33fddc36b365b772a0254
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 09:31:03 +0200

    formatting fixes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -59,7 +59,7 @@ const Signup = () => {
   }, []);
 
   return (
-    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+    <div className='container flex items-center h-screen max-w-screen-md mx-auto'>
       <div className='flex w-3/5'>
         <img
           src='/images/iphone-with-profile.jpg'
@@ -67,12 +67,12 @@ const Signup = () => {
         />
       </div>
       <div className='flex flex-col w-2/5'>
-        <div className='flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded'>
+        <div className='flex flex-col items-center p-4 mb-4 bg-white border rounded border-gray-primary'>
           <h1 className='flex justify-center w-full'>
             <img
               src='/images/logo.png'
               alt='Instagram'
-              className='mt-2 w-6/12 mb-4'
+              className='w-6/12 mt-2 mb-4'
             />
           </h1>
 
@@ -83,7 +83,7 @@ const Signup = () => {
               aria-label='Enter your username'
               type='text'
               placeholder='Username'
-              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+              className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary'
               onChange={({ target }) => setUsername(target.value)}
               value={username}
             />
@@ -91,7 +91,7 @@ const Signup = () => {
               aria-label='Enter your full name'
               type='text'
               placeholder='Full name'
-              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+              className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary'
               onChange={({ target }) => setFullName(target.value)}
               value={fullName}
             />
@@ -99,7 +99,7 @@ const Signup = () => {
               aria-label='Enter your email address'
               type='text'
               placeholder='Email address'
-              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+              className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary'
               onChange={({ target }) => setEmailAddress(target.value)}
               value={emailAddress}
             />
@@ -107,7 +107,7 @@ const Signup = () => {
               aria-label='Enter your password'
               type='password'
               placeholder='Password'
-              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+              className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary'
               onChange={({ target }) => setPassword(target.value)}
               value={password}
             />
@@ -121,7 +121,7 @@ const Signup = () => {
             </button>
           </form>
         </div>
-        <div className='flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary'>
+        <div className='flex flex-col items-center justify-center w-full p-4 bg-white border rounded border-gray-primary'>
           <p className='text-sm'>
             Have an account?{` `}
             <Link to={ROUTES.LOGIN} className='font-bold text-blue-medium'>
