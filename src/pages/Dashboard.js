96d1b0eca9e2d354df4dc6cd9999b4a713935a3c
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 09:47:43 +0200

    added grid container

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,7 +5,11 @@ const Dashboard = () => {
     document.title = 'Instagram';
   }, []);
 
-  return <div className='bg-gray-background'></div>;
+  return (
+    <div className='bg-gray-background'>
+      <div className='grid'></div>
+    </div>
+  );
 };
 
 export default Dashboard;
