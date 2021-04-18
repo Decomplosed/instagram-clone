86b248aaf6151296030ea3ff95ccf933e7578374
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 09:11:28 +0200

    formatting fixes; adjusted the submit button for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
   }, []);
 
   return (
-    <div className='container flex mx-auo max-w-screen-md items-center h-screen'>
+    <div className='container flex items-center h-screen max-w-screen-md mx-auto'>
       <div className='flex w-3/5'>
         <img
           src='/images/iphone-with-profile.jpg'
@@ -39,12 +39,12 @@ const Login = () => {
         />
       </div>
       <div className='flex flex-col w-2/5'>
-        <div className='flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded'>
+        <div className='flex flex-col items-center p-4 mb-4 bg-white border rounded border-gray-primary'>
           <h1 className='flex justify-center w-full'>
             <img
               src='/images/logo.png'
               alt='Instagram'
-              className='mt-2 w-6/12 mb-4'
+              className='w-6/12 mt-2 mb-4'
             />
           </h1>
           {error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
@@ -54,28 +54,27 @@ const Login = () => {
               aria-label='Enter your email address'
               type='text'
               placeholder='Email Address'
-              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+              className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary'
               onChange={({ target }) => setEmailAddress(target.value)}
             />
             <input
               aria-label='Enter your password'
               type='password'
               placeholder='Password'
-              className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+              className='w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary'
               onChange={({ target }) => setPassword(target.value)}
             />
             <button
               disabled={isInvalid}
               type='submit'
-              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
-                isInvalid && ' opacity-50'
-              }`}
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+            ${isInvalid && 'opacity-50'}`}
             >
               Login
             </button>
           </form>
         </div>
-        <div className='flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded'>
+        <div className='flex flex-col items-center justify-center w-full p-4 bg-white border rounded border-gray-primary'>
           <p className='text-sm'>
             Don't have an account?{` `}
             <Link to={ROUTES.SIGN_UP} className='font-bold text-blue-medium'>
