eb7676a59d51489cbe5807546b140d9cdca86b7f
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 09:35:24 +0200

    added content

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div className='bg-gray-background'>
-      <div className='max-w-screen-lg mx-auto'></div>
+      <div className='max-w-screen-lg mx-auto'>
+        <p className='text-2xl text-center'>404 | Not Found</p>
+      </div>
     </div>
   );
 }
