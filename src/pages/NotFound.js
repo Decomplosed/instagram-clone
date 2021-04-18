0dded39038c0b5fd057e0e92d8708769f378c7a1
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 09:41:52 +0200

    changed body of component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const NotFound = () => (
-  <div className='bg-gray-background'>
-    <div className='max-w-screen-lg mx-auto'>
-      <p className='text-2xl text-center'>404 | Not Found</p>
+const NotFound = () => {
+  return (
+    <div className='bg-gray-background'>
+      <div className='max-w-screen-lg mx-auto'>
+        <p className='text-2xl text-center'>404 | Not Found</p>
+      </div>
     </div>
-  </div>
-);
-
-export default NotFound;
+  );
+};
+export defautl NotFound
