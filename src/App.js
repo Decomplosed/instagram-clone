a93f9bdbea0265c648f3767c0ae4b7cbef33a0bb
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-18 08:59:40 +0200

    added Route to Signup page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Suspense fallback={<p>Loading ...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={Signup} />
         </Switch>
       </Suspense>
     </Router>
