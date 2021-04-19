148703e9f69f8cbedf041ef715a6fc5f381a001c
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-19 07:45:03 +0200

    imported Timeline component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Header from '../components/Header';
+import Timeline from '../components/Timeline';
 
 const Dashboard = () => {
   useEffect(() => {
