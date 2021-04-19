eced886ab69fea9805f9b22c3d92ddf225586ef3
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-19 07:53:40 +0200

    useState case for user value

--- src/hooks/UseAuthListener.js
+++ src/hooks/UseAuthListener.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
-const UseAuthListener = () => {};
+const UseAuthListener = () => {
+  const [user, setUser] = useState()
+
+};
 
 export default UseAuthListener;
