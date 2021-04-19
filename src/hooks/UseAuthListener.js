169106e540e9d46dbbf6a7e6dab97bcf0c4ca897
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-19 07:53:10 +0200

    export out UseAuthListener

--- src/hooks/UseAuthListener.js
+++ src/hooks/UseAuthListener.js
@@ -2,3 +2,5 @@ import React, { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 const UseAuthListener = () => {};
+
+export default UseAuthListener;
