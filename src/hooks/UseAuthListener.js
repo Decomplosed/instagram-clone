b1d500bf0a1bf90190989fa5a9a4034342c24f7c
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-19 07:54:03 +0200

    get user from localStorage

--- src/hooks/UseAuthListener.js
+++ src/hooks/UseAuthListener.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 const UseAuthListener = () => {
-  const [user, setUser] = useState()
-
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem('authUser')),
+  );
 };
 
 export default UseAuthListener;
