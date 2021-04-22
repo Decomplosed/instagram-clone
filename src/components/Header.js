44379760f51f3fae619ba4b0b5ae70cbf55fed85
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-22 16:04:26 +0200

    imported ROTUES

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
+import * as ROTUES from '../constants/routes';
 
 const Header = () => {
   const { firebase } = useContext(FirebaseContext);
