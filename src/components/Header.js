454e78db2ba92538825c97f3308f130ea6db67a9
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-22 16:04:46 +0200

    imported Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 import * as ROTUES from '../constants/routes';
