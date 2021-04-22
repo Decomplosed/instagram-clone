7bf104a2f8bc86b9bc760911a38b043e752df33b
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-22 16:01:26 +0200

    imported UserContext

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
 
 const Header = () => {
   return <div>HEADER COMPONENT</div>;
