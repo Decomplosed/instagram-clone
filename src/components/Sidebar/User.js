7bf9414f3df84b3a1f3434e4a2c2d609532777d4
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-26 15:48:24 +0200

    added isRequired flag to propTypes

--- src/components/Sidebar/User.js
+++ src/components/Sidebar/User.js
@@ -23,8 +23,8 @@ const User = ({ username, fullName }) =>
   );
 
 User.propTypes = {
-  username: PropTypes.string,
-  fullName: PropTypes.string,
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired,
 };
 
 export default User;
