d10bbedc3f6c778008e9539e692972b6f315b871
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-26 08:49:37 +0200

    show different templates based on username or fullName value

--- src/components/Sidebar/User.js
+++ src/components/Sidebar/User.js
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 
-const User = ({ username, fullName }) => {
-  return <p>USER COMPONENT</p>;
-};
+const User = ({ username, fullName }) =>
+  !username || !fullName ? <Skeleton /> : <Skeleton></Skeleton>;
 
 User.propTypes = {
   username: PropTypes.string,
