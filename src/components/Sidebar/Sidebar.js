8ba86fc7b9c1d5945475e0491836f995318473dd
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-26 08:37:14 +0200

    moved Sidebar to Sidebar folder and created file for User component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useUser from '../hooks/useUser';
+import useUser from '../../hooks/useUser';
 import User from './User';
 import Suggestions from './Suggestions';
 
