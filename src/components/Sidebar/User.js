beeb0725f473f47965c4ea734bd7ea57a6cc576a
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-26 08:46:11 +0200

    imported Skeleton

--- src/components/Sidebar/User.js
+++ src/components/Sidebar/User.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 
 const User = ({ username, fullName }) => {
   return <p>USER COMPONENT</p>;
