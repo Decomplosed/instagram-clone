790a6082b2591d3c0e03443779c9e0243040f78b
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-26 08:47:46 +0200

    added propTypes for User component

--- src/components/Sidebar/User.js
+++ src/components/Sidebar/User.js
@@ -7,4 +7,9 @@ const User = ({ username, fullName }) => {
   return <p>USER COMPONENT</p>;
 };
 
+User.propTypes = {
+  username: PropTypes.string,
+  fullName: PropTypes.string,
+};
+
 export default User;
