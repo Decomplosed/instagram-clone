7322cdaa05e19f374bf38fbaddf44bbefe806e6a
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-28 21:40:22 +0200

    render fullName

--- src/components/Sidebar/User.js
+++ src/components/Sidebar/User.js
@@ -20,6 +20,7 @@ const User = ({ username, fullName }) =>
       </div>
       <div className='col-span-3'>
         <p className='text-sm font-bold'>{username}</p>
+        <p className='text-sm font-bold'>{fullName}</p>
       </div>
     </Link>
   );
