0d39c1f2ab96e7a429147a5e1b2050e1ede05ad1
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-28 22:02:58 +0200

    styled sub container

--- src/components/Sidebar/Suggestions.js
+++ src/components/Sidebar/Suggestions.js
@@ -9,7 +9,7 @@ const Suggestions = () => {
     <Skeleton count={1} height={150} className='mt-5' />
   ) : profiles.length > 0 ? (
     <div className='flex flex-col rounded'>
-      <div></div>
+      <div className='flex items-center justify-between mb-2 text-sm align-items'></div>
       <p className='text-bold text-gray-base'>Suggestions for you</p>
     </div>
   ) : null;
