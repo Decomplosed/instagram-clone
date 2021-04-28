cfc76de9122bc427fe60ab19dee2324a98103b85
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-28 21:58:36 +0200

    imported Skeleton

--- src/components/Sidebar/Suggestions.js
+++ src/components/Sidebar/Suggestions.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
 
 const Suggestions = () => {
   const [profiles, setProfiles] = useState(null);
