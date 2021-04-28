b43a0fc01cf6b288fad6b8e90c758655fc4edccf
Bart Frackiewicz <decomplosed@gmail.com>
2021-04-28 22:10:54 +0200

    import getSuggestedProfiles

--- src/components/Sidebar/Suggestions.js
+++ src/components/Sidebar/Suggestions.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
+import { getSuggestedProfiles } from '../../services/firebase';
 
 const Suggestions = () => {
   const [profiles, setProfiles] = useState(null);
